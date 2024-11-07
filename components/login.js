0f398bb0ddb634cbc871862e2a1524876952d13b
Dan Chen <dan.chen01@edu.sait.ca>
2024-11-07 15:20:32 -0700

    add react-native-google-signin and configuration app.json

--- components/login.js
+++ components/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import { firebase } from "@react-native-firebase/auth";
 
 export default function Login() {
   return <View style={styles.container}></View>;
